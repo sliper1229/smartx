69a46ce854991bc254d03ab43fd75751bf01ddf9
sliper1229 <675754935@qq.com>
2021-11-05 09:07:49 +0800

    20211105

--- smartx-algorithm/src/main/java/com/cc/smartx/algorithm/zuochengyun/leetcodeTopInterview/topinterviewquestions/leetcode/高频题目全讲_8/Problem_0053_MaximumSubarray.java
+++ smartx-algorithm/src/main/java/com/cc/smartx/algorithm/zuochengyun/leetcodeTopInterview/topinterviewquestions/leetcode/高频题目全讲_8/Problem_0053_MaximumSubarray.java
@@ -1,7 +1,8 @@
-package com.cc.smartx.algorithm.zuochengyun.leetcodeTopInterview.topinterviewquestions;
+package com.cc.smartx.algorithm.zuochengyun.leetcodeTopInterview.topinterviewquestions.leetcode.高频题目全讲_8;
 
 public class Problem_0053_MaximumSubarray {
 
+	// 最大子序和
 	public static int maxSum(int[] nums) {
 		int N = nums.length;
 		// dp[i] 含义：子数组必须以i结尾的时候，所有可以得到的子数组中，最大累加和是多少？
@@ -9,7 +10,10 @@ public class Problem_0053_MaximumSubarray {
 		dp[0] = nums[0];
 		int max = Integer.MIN_VALUE;
 		for (int i = 1; i < N; i++) {
+			// 列举可能性
+			// 子数组以i结尾，但是只包含i
 			int p1 = nums[i];
+			// 子数组以i结尾，但是值包含i [......i-1] + nums[i]
 			int p2 = nums[i] + dp[i - 1];
 			dp[i] = Math.max(p1, p2);
 			max = Math.max(max, dp[i]);
@@ -17,6 +21,18 @@ public class Problem_0053_MaximumSubarray {
 		return max;
 	}
 
+	public static int maxSubArray(int[] nums) {
+		int cur = 0;
+		int max = Integer.MIN_VALUE;
+		for (int i = 0; i < nums.length; i++) {
+			cur += nums[i];
+			max = Math.max(max, cur);
+			cur = cur < 0 ? 0 : cur;
+		}
+		return max;
+	}
+
+	// 选组合，不能同时选相邻的数，哪个组合累加和最大
 	public static int maxSum2(int[] arr) {
 		if (arr == null) {
 			return 0;
@@ -32,27 +48,21 @@ public class Problem_0053_MaximumSubarray {
 			return Math.max(arr[0], arr[1]);
 		}
 		// N > 2
+		// dp[i] 含义：[0.....i]所有可能性的最优，所以最大累加和肯定是最后N-1位置，不要求必须以i结尾
 		int[] dp = new int[N];
 		dp[0] = arr[0];
 		dp[1] = Math.max(arr[0], arr[1]);
 		for (int i = 2; i < N; i++) {
+			// 只含i
 			int p1 = arr[i];
+			// 不含i
 			int p2 = dp[i - 1];
+			// 选i，不选i-1，选i-2
 			int p3 = arr[i] + dp[i - 2];
 			dp[i] = Math.max(Math.max(p1, p2), p3);
 		}
+		//
 		return dp[N-1];
-	}
-
-	public static int maxSubArray(int[] nums) {
-		int cur = 0;
-		int max = Integer.MIN_VALUE;
-		for (int i = 0; i < nums.length; i++) {
-			cur += nums[i];
-			max = Math.max(max, cur);
-			cur = cur < 0 ? 0 : cur;
-		}
-		return max;
 	}
 
 }
--- smartx-algorithm/src/main/java/com/cc/smartx/algorithm/zuochengyun/leetcodeTopInterview/topinterviewquestions/leetcode/高频题目全讲_8/Problem_0062_UniquePaths.java
+++ smartx-algorithm/src/main/java/com/cc/smartx/algorithm/zuochengyun/leetcodeTopInterview/topinterviewquestions/leetcode/高频题目全讲_8/Problem_0062_UniquePaths.java
@@ -1,15 +1,23 @@
-package com.cc.smartx.algorithm.zuochengyun.leetcodeTopInterview.topinterviewquestions;
+package com.cc.smartx.algorithm.zuochengyun.leetcodeTopInterview.topinterviewquestions.leetcode.高频题目全讲_8;
 
 public class Problem_0062_UniquePaths {
 
 	public static int uniquePaths(int m, int n) {
 		int part = n - 1;
+		// 需要走的总步数
 		int all = m + n - 2;
 		long o1 = 1;
 		long o2 = 1;
+
+		// C(9,3) = 9的阶乘 / 3的阶乘 /（9-3）的阶乘
+		// all = 9 part = 9 - 3 = 6
+		// part + 1 = 7.......9
+		// all - part = 1.......3
+		// 7.......9 / 1.......3
 		for (int i = part + 1, j = 1; i <= all || j <= all - part; i++, j++) {
 			o1 *= i;
 			o2 *= j;
+			// 求最大公约数
 			long gcd = gcd(o1,o2);
 			o1 /= gcd;
 			o2 /= gcd;
@@ -18,6 +26,7 @@ public class Problem_0062_UniquePaths {
 	}
 	
 	// 调用的时候，请保证初次调用时，m和n都不为0
+	// 转转相除法
 	public static long gcd(long m, long n) {
 		return n == 0 ? m : gcd(n, m % n);
 	}
